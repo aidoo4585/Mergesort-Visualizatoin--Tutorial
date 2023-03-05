e55fcdfb8a03303b31e9592e3d9089d5a2f24507
Kenneth Aidoo <46071969+aidoo4585@users.noreply.github.com>
2023-03-05 10:58:10 -0500

    Added sort via upload

---
+++ lil-einsteins/src/Components/NavbarComponent.js
@@ -0,0 +1,31 @@
+import {React} from "react";
+import {Navbar, Nav, Container} from "react-bootstrap";
+import "./Components.css";
+import {useLocation} from "react-router-dom";
+
+export default function Component(props) {
+	return (
+		<Navbar className="color-nav m" expand="lg">
+			<Container>
+				<img
+					src={require("../images/logo.png")}
+					alt="logo"
+					width="20"
+					height="20"
+					className="me-2"
+				/>
+				<Navbar.Brand href="Level1">Lil' Einsteins</Navbar.Brand>
+
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="ms-auto">
+						<Nav.Link href="Level1">Merge Sort</Nav.Link>
+						<Nav.Link href="#quick"></Nav.Link>
+						<Nav.Link href="#shell"></Nav.Link>
+					</Nav>
+					<Navbar.Brand>{props.level}</Navbar.Brand>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
+}
