9a333430dc26969e9eda7eb8418c51dbd7085186
Kenneth Aidoo <46071969+aidoo4585@users.noreply.github.com>
2023-03-05 10:45:19 -0500

    Uploaded merge sort project via upload

---
+++ mergesort-visualiztion-tutorial/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import NavbarComponent from "./components/NavbarComponent";
+import Footer from "./components/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+	return (
+		<div>
+			<NavbarComponent />
+			<Footer />
+		</div>
+	);
+}
+
+export default App;
